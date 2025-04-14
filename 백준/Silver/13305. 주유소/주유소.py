5ca6973a3dfea2837b8c682464ac7d2ecc192a4e
SeungMin Oh <95595047+HiSeungmin@users.noreply.github.com>
2025-04-14 23:23:16 +0900

    [Silver III] Title: 주유소, Time: 116 ms, Memory: 47580 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -0,0 +1,20 @@
+N     = int(input())
+road  = list(map(int, input().split()))
+price = list(map(int, input().split()))
+
+road.append(0)
+price[-1] = 0
+
+sum    = 0
+mile   = road[0]
+target = price[0]
+for k in range(N-1):
+    if target < price[k+1]:
+        mile += road[k+1]
+    else:
+        sum += (target*mile)
+        target = price[k+1]
+        mile = road[k+1]
+    
+
+print(sum)
