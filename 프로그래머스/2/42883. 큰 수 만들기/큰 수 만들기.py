8fac04155b8d5c35ac407b0f7288e678fe2544da
SeungMin Oh <95595047+HiSeungmin@users.noreply.github.com>
2025-04-24 14:47:39 +0900

    [level 2] Title: 큰 수 만들기, Time: 85.51 ms, Memory: 12 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,12 +1,12 @@
 def solution(number, k):
+
     stack = []
-    
-    for i in number:
-        while stack and stack[-1] < i and k > 0:
+    for v in number:
+        while stack and stack[-1] < v and k>0:
             stack.pop()
             k -= 1
-        stack.append(i)
-    
-    if k > 0 : stack = stack[:-k]
-    
+        stack.append(v)
+
+    if k > 0:
+        stack = stack[:-k]
     return ''.join(stack)
