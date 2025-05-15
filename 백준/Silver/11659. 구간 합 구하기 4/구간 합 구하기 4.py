133082cf95ad4c61ac542e4060c74de1b699154f
SeungMin Oh <95595047+HiSeungmin@users.noreply.github.com>
2025-05-15 17:02:03 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 228 ms, Memory: 41144 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -0,0 +1,14 @@
+import sys
+N, M = map(int, sys.stdin.readline().split(' '))
+arr = list(map(int,sys.stdin.readline().split(' ')))
+sum = 0
+
+new_arr = [0] 
+for k in range(N):
+    sum += arr[k]
+    new_arr.append(sum)
+
+
+for _ in range(M):
+    a, b = map(int, sys.stdin.readline().split(' '))
+    print(new_arr[b]-new_arr[a-1])
