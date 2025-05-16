f4b217b5b3c28738e531a7bd44856b2d0da66295
SeungMin Oh <95595047+HiSeungmin@users.noreply.github.com>
2025-05-16 19:17:09 +0900

    [level 2] Title: 더 맵게, Time: 1516.03 ms, Memory: 123 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int ans = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i :scoville){
+            pq.add(i);
+        }
+        int mix = -1;
+        while (pq.peek() < K) {
+            if (pq.size() < 2) {
+                return -1;
+            }
+            int first = pq.poll();
+            int second = pq.poll();
+            mix = first + second * 2;
+            pq.add(mix);
+            ans++;
+        }
+        return ans;
+    }
+}
