32126be0d53b3cf3bcb6afeeafd98f826246dcfc
SeungMin Oh <95595047+HiSeungmin@users.noreply.github.com>
2025-07-14 14:40:32 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 75.87 ms, Memory: 143 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -1,32 +1,31 @@
 import java.util.*;
 class Solution {
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount)    {
-         int[] answer = new int[enroll.length];
+        int[] answer = new int[enroll.length];
         Map<String, String> map = new HashMap<>();
         Map<String, Integer> cash = new HashMap<>();
 
-        for (int i = 0; i < enroll.length ; i++) {
+        for(int i=0; i<enroll.length; i++){
             map.put(enroll[i], referral[i]);
         }
 
-        for (int i = 0; i <seller.length ; i++) {
-            String s = seller[i];
-            int a = amount[i]*100;
-            
+        for(int k=0; k< seller.length; k++){
+            String sel = seller[k];
+            int amnt = amount[k]*100;
+
             while(true){
-                cash.put(s,cash.getOrDefault(s,0)+(a-(int)(a*0.1)));
-                a = (int)(a*0.1);
-
-                if(!map.containsKey(s) || map.get(s).equals("-")|| a<1) break;
-                s = map.get(s);
+                cash.put(sel, cash.getOrDefault(sel,0)+amnt-(int)(amnt*0.1));
+                amnt *= 0.1;
+                if(amnt<1||!map.containsKey(sel)||map.get(sel) == "-"){
+                    break;
+                }
+                sel = map.get(sel);
             }
-
-        }
-        
-        for (int i = 0; i <enroll.length ; i++) {
-            answer[i] =cash.containsKey(enroll[i])?cash.get(enroll[i]):0;
         }
 
+        for(int i=0; i<enroll.length; i++){
+            answer[i] = cash.containsKey(enroll[i])?cash.get(enroll[i]):0;
+        }
         return answer;
     }
 }
