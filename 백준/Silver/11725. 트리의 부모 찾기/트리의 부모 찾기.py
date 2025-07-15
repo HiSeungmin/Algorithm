5e2c6555cb32c2d3d7803ab37d00b29f6aa200db
SeungMin Oh <95595047+HiSeungmin@users.noreply.github.com>
2025-07-15 15:48:14 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 252 ms, Memory: 130528 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,21 +1,26 @@
-import sys
-sys.setrecursionlimit(10**5)
-N = int(sys.stdin.readline())
-tree = [[] for _ in range(N+1)]
+# 트리의 부모 찾기
 
-for _ in range(N-1):
-    a, b = map(int, input().split())
-    tree[a].append(b)
-    tree[b].append(a)
+node_num = int(input())
+arr = [[] for _ in range(node_num+1)]
+for i in range(node_num-1):
+    a, b = map(int,input().split())
+    arr[a].append(b)
+    arr[b].append(a)
 
-checked = [0] * (N+1)
-def dfs(x):
-    for i in tree[x]:
-        if checked[i] == 0:
-            checked[i] = x
-            dfs(i)
+visited = [0 for _ in range(node_num+1)]
+parent = [0 for _ in range(node_num+1)]
+q = []
+q.append(1)
 
-dfs(1)
+while q:
+    x = q.pop()
+    if visited[x] != 1:
+        visited[x] = 1
+        
+        for k in arr[x] : 
+            if parent[k] == 0:
+                parent[k] = x
+            q.append(k)
 
-for k in range(2, N+1):
-    print(checked[k])
+for i in range(2, node_num+1):
+    print(parent[i])
