e081978875493a42fb8be611e60b2150dbd2ea20
SeungMin Oh <95595047+HiSeungmin@users.noreply.github.com>
2025-08-04 18:55:41 +0900

    [Gold V] Title: 강의실 배정, Time: 572 ms, Memory: 154412 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.py
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -1,21 +1,18 @@
 import heapq
 n = int(input())
-q = []
-
-for i in range(n):
-    start, end = map(int, input().split())
-    q.append([start, end])
-
-q.sort()
-
+arr =[]
 room = []
-heapq.heappush(room, q[0][1])
+for _ in range(n):
+    arr.append(list(map(int,input().split(' '))))
 
-for i in range(1, n):
-    if q[i][0] < room[0]:
-        heapq.heappush(room, q[i][1])
-    else: 
+arr.sort()
+heapq.heappush(room, arr[0][1])
+
+for k in range(1,n):
+    if arr[k][0] < room[0]:
+        heapq.heappush(room,arr[k][1])
+    else:
         heapq.heappop(room)
-        heapq.heappush(room, q[i][1])
+        heapq.heappush(room,arr[k][1])
 
 print(len(room))
