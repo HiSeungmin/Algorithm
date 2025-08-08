27c66969e7307809c7e3c21a47c8cb56d649fbda
SeungMin Oh <95595047+HiSeungmin@users.noreply.github.com>
2025-08-08 21:31:59 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 16.81 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -1,66 +1,67 @@
 import java.util.*;
 class Solution {
-    private static class Node{
-        int x, y, direction, cost;
+    public static class Node{
+        int x;
+        int y;
+        int direction;
+        int cost;
         public Node(int x, int y, int direction, int cost){
             this.x=x;
             this.y=y;
-            this.direction = direction;
-            this.cost = cost;
+            this.direction=direction;
+            this.cost=cost;
         }
     }
 
-    private static final int[] rx = {0, -1, 0, 1};
-    private static final int[] ry = {-1, 0, 1, 0};
+    private static int[] dx = {0, -1, 0, 1};
+    private static int[] dy = {-1, 0, 1, 0};
+    private static int[][] graph;
     private static int N;
     private static int[][][] visited;
 
     private static boolean isValid(int x, int y){
-        return 0<=x&&x<N&&0<=y&&y<N;
+        return 0<=x && x<N && 0<=y && y<N;
     }
 
-    private static boolean isBlocked(int[][] board, int x, int y){
-        return (x==0&&y==0) || !isValid(x,y) || board[x][y] == 1;
+    private static boolean isBlocked(int x, int y){
+        return graph[x][y]==1;
     }
 
-    private static int calculateCost(int direction, int prevDirection, int cost){
-        if(prevDirection == -1 || (prevDirection - direction)%2 == 0){
+    private static int calculator(int direction, int prevDirection, int cost){
+        if(direction == -1 || (prevDirection-direction)%2==0){
             return cost+100;
         }
         return cost+600;
     }
 
-    private static boolean isShouldUpdate(int x, int y, int direction,int newCost){
-        return visited[x][y][direction] == 0 || visited[x][y][direction] > newCost;
-    }
-
     public static int solution(int[][] board) {
-        ArrayDeque<Node> queue = new ArrayDeque<>();
-        queue.add(new Node(0,0,-1,0));
-        N = board.length;
-        visited = new int[N][N][4];
-
         int answer = Integer.MAX_VALUE;
+        N = board.length;
+        graph = board;
+        visited = new int[N][N][4];
+        Deque<Node> queue = new ArrayDeque<>();
+        queue.add(new Node(0, 0, -1, 0));
 
         while(!queue.isEmpty()){
             Node now = queue.poll();
 
             for(int i=0; i<4; i++){
-                int new_x = now.x + rx[i];
-                int new_y = now.y + ry[i];
+                int nx = now.x+dx[i];
+                int ny = now.y+dy[i];
 
-                if(isBlocked(board, new_x, new_y)){
+                if(!isValid(nx,ny)|| isBlocked(nx,ny)){
                     continue;
                 }
 
-                int new_cost = calculateCost(i, now.direction, now.cost);
+                int newCost = calculator(now.direction, i, now.cost);
 
-                if(new_x == N-1 && new_y == N-1){
-                    answer = Math.min(answer, new_cost);
-                }else if(isShouldUpdate(new_x,new_y,i,new_cost)){
-                    queue.add(new Node(new_x, new_y, i, new_cost));
-                    visited[new_x][new_y][i] = new_cost;
+                if(nx == N-1 && ny == N-1){
+                    answer = Math.min(answer, newCost);
+                }else if (visited[nx][ny][i] == 0 || visited[nx][ny][i] > newCost){
+                    queue.add(new Node(nx,ny,i,newCost));
+                    visited[nx][ny][i] = newCost;
                 }
+
             }
         }
         return answer;
