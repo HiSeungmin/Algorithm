8666b3eab17098b3e77332697f72aee555e9b39b
SeungMin Oh <95595047+HiSeungmin@users.noreply.github.com>
2025-08-27 11:38:50 +0900

    [level 1] Title: K번째수, Time: 0.53 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,21 +2,11 @@ import java.util.*;
 class Solution {
     public static int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        int b = 0;
-        for(int[] com : commands){
-            int i = com[0]-1;
-            int j = com[1];
-            int k = com[2]-1;
-            int[] arr = new int[j-i];
-            int a=0;
-            for(int s = i; s<j; s++){
-                arr[a++] = array[s];
-            }
 
-            Arrays.sort(arr);
-            System.out.println(Arrays.toString(arr));
-            answer[b++] = arr[k];
-
+        for(int i=0; i<commands.length; i++){
+            int[] temp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2]-1];
         }
 
         return answer;
