a24ecbec8d94c3ab77aada9a7a7831ac3fdc3ec0
SeungMin Oh <95595047+HiSeungmin@users.noreply.github.com>
2025-09-30 11:18:59 +0900

    [Silver III] Title: 계단 오르기, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -0,0 +1,14 @@
+N = int(input())
+stairs = [0]*301
+for i in range(1,N+1):
+    stairs[i] = int(input())
+
+dp = [0]*301
+dp[1] = stairs[1]
+dp[2] = stairs[1]+stairs[2]
+dp[3] = max(stairs[1]+stairs[3], stairs[2]+stairs[3])
+
+for k in range(4,N+1):
+    dp[k] = max(dp[k-3]+stairs[k-1]+stairs[k], dp[k-2]+stairs[k])
+
+print(dp[N])
