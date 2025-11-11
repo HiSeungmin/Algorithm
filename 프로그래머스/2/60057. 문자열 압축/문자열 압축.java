cc409f5d5e58e9302c9bf622fe36f95986ea9689
SeungMin Oh <95595047+HiSeungmin@users.noreply.github.com>
2025-11-11 09:46:13 +0900

    [level 2] Title: 문자열 압축, Time: 20.26 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -0,0 +1,35 @@
+class Solution {
+    public static int solution(String s) {
+        int answer = Integer.MAX_VALUE;
+        int len = s.length();
+        if(s.length()==1) return 1;
+
+        for(int r=1; r<=len/2; r++){
+            String pattern = s.substring(0,r);
+            int cnt = 1;
+            String result = "";
+
+            for(int i=r; i<=s.length()-r; i+=r){
+                if(pattern.equals(s.substring(i, i+r))){
+                    cnt++;
+                }else{
+                    if(cnt>1){
+                        result += cnt+"";
+                    }
+                    result += pattern;
+                    pattern = s.substring(i,i+r);
+                    cnt=1;
+                }
+            }
+            if(cnt>1){
+                result += ""+cnt;
+            }
+            result += pattern;
+
+            int div = s.length()%r;
+            answer = Math.min(answer, result.length()+div);
+        }
+
+        return answer;
+    }
+}
