a9dcac676062d7ed278e247b1495d54c532e6f8c
SeungMin Oh <95595047+HiSeungmin@users.noreply.github.com>
2026-01-06 17:53:28 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 지폐 접기, Time: 0.03 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public static int solution(int[] wallet, int[] bill) {
+        int answer = 0;
+
+        while(true){
+            if((bill[0]<=wallet[0] && bill[1]<=wallet[1]) ||
+                    (bill[1]<=wallet[0] && bill[0]<=wallet[1])){
+                return answer;
+            }
+
+
+            if(bill[0]<bill[1]){
+                bill[1] = bill[1]/2;
+            }else{
+                bill[0] = bill[0]/2;
+            }
+            answer += 1;
+        }
+
+    }
+}
