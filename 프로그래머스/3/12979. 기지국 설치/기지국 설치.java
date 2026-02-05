aab4e4557b8d34eff4355c5789eb299324775514
SeungMin Oh <95595047+HiSeungmin@users.noreply.github.com>
2026-02-06 00:00:16 +0900

    [level 3] Title: 기지국 설치, Time: 0.70 ms, Memory: 54.1 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -1,19 +1,28 @@
 class Solution {
     public static int solution(int n, int[] stations, int w) {
         int answer = 0;
-        int range = (w*2)+1;
+        int ran = (w*2)+1;
         int s = 0;
-        for(int i: stations){
+
+        for(int i:stations){
             int e = i-w-1;
 
             if(e>0){
-                answer += (e-s)%range>0? ((e-s)/range)+1:((e-s)/range);
+                if((e-s)%ran>0){
+                    answer += 1;
+                }
+                 answer += (e-s)/ran;
             }
+
             s = i+w;
         }
         if(s<n){
-            answer += (n-s)%range>0? ((n-s)/range)+1:((n-s)/range);
+            if((n-s)%ran>0){
+                answer += 1;
+            }
+            answer += (n-s)/ran;
         }
+
         return answer;
     }
 }
