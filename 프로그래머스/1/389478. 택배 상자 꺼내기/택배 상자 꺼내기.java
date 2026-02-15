9b6dd1ba31aa7fea1db2ab4eaf65dfcbfec963e8
SeungMin Oh <95595047+HiSeungmin@users.noreply.github.com>
2026-02-15 21:37:17 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.03 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -1,20 +1,24 @@
 class Solution {
     public int solution(int n, int w, int num) {
-        int answer = 0;
-        int col = (num-1)/w; 
-        int row = w-1; 
-        
-        if(col%2==0){
-            row = (num-1)%w;
-        }else{
-            row = w-1 - (num-1)%w;
+        int answer = 0; 
+        int row = (num-1)/w; 
+        int col = w-1; 
+        int pin = 0; 
+
+        if (row%2 == 0) { 
+            pin = (num-1) % w;
+        } else { 
+            pin = col - (num-1) % w;
         }
-        
-        for(int i=num; i<=n; i++){
-            int max_col = (i-1)/w;
-            if(max_col%2==0 && row==(i-1)%w){
-                answer++;
-            }else if(max_col%2!=0 && row==(w-1 - (i-1)%w)){
+
+        for (; row < (n+col)/w; row++) { 
+            int aboveBox = 0; 
+            if (row % 2 == 0) { 
+                aboveBox = pin + (row * w);
+            } else { 
+                aboveBox = (col - pin) + (row * w);
+            }
+            if (aboveBox < n) {
                 answer++;
             }
         }
