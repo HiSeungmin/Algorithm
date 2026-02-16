5cb583e720978a24b4d3e86494b54859ad95dce8
SeungMin Oh <95595047+HiSeungmin@users.noreply.github.com>
2026-02-16 21:01:37 +0900

    [level 3] Title: 숫자 게임, Time: 69.10 ms, Memory: 70.5 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -2,27 +2,18 @@ import java.util.*;
 class Solution {
     public int solution(int[] A, int[] B) {
 
-        PriorityQueue<Integer> pq = new PriorityQueue(Collections.reverseOrder());
-        for(int i : A)
-            pq.add(i);
-        
-        Arrays.sort(B);
-        ArrayDeque<Integer> dq = new ArrayDeque();
-        for(int i : B)
-            dq.add(i);
-        
         int answer = 0;
-        while(!pq.isEmpty()){
-            int target = pq.poll();
-            
-            if(target < dq.peekLast()){
-                dq.pollLast();
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        for (int i = A.length - 1, j = B.length - 1; i >= 0; i--) {
+            if(A[i] < B[j]) {
                 answer++;
-            }else{ 
-                dq.pollFirst();
+                j--;
             }
         }
-        
+
         return answer;
     }
 }
