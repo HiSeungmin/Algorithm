3c4d0e0cbdbc7a9cc3862d57f23d9357cd86cfde
SeungMin Oh <95595047+HiSeungmin@users.noreply.github.com>
2026-03-10 14:14:00 +0900

    [level 1] Title: 노란불 신호등, Time: 54.22 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.java
+++ 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.java
@@ -1,27 +1,24 @@
 class Solution {
     public int solution(int[][] signals) {
-        for (int t = 1; t <= 5000000; t++) {
-
-            boolean allYellow = true;
-
-            for (int[] s : signals) {
-
+        for(int t=1; t<5000000; t++){
+            boolean isYellow = true;
+            
+            for(int[] s : signals){
                 int g = s[0];
                 int y = s[1];
                 int r = s[2];
-
-                int cycle = g + y + r;
-                int mod = t % cycle;
-
-                if (!(mod >= g && mod < g + y)) {
-                    allYellow = false;
+                
+                int cycle = g+y+r;
+                int mod = t%cycle;
+                
+                if(!(mod>g && mod <= g+y)){
+                    isYellow = false;
                     break;
                 }
             }
-
-            if (allYellow) return t+1;
+            
+            if(isYellow) return t;
         }
-
         return -1;
     }
 }
