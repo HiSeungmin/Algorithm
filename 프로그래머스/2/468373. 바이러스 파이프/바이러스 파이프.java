852b40d0d5303e5dfb343ffaecacea31f531c280
SeungMin Oh <95595047+HiSeungmin@users.noreply.github.com>
2026-04-10 23:58:41 +0900

    [level 2] Title: 바이러스 파이프, Time: 19.95 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/2/468373. 바이러스 파이프/바이러스 파이프.java
+++ 프로그래머스/2/468373. 바이러스 파이프/바이러스 파이프.java
@@ -1,83 +1,75 @@
 import java.util.*;
 class Solution {
-    static class Edge {
+    static class Edge{
         int to;
         int type;
 
-        Edge(int to, int type) {
+        Edge(int to, int type){
             this.to = to;
             this.type = type;
         }
     }
 
     int n, k;
+    int answer;
     List<List<Edge>> graph;
-    int answer = 0;
 
     public int solution(int n, int infection, int[][] edges, int k) {
-
         this.n = n;
         this.k = k;
+        answer = 0;
 
         graph = new ArrayList<>();
 
-        for (int i = 0; i <= n; i++)
+        for(int i=0; i<=n; i++){
             graph.add(new ArrayList<>());
-
-        for (int[] e : edges) {
-
-            int a = e[0];
-            int b = e[1];
-            int type = e[2];
-
-            graph.get(a).add(new Edge(b, type));
-            graph.get(b).add(new Edge(a, type));
         }
 
-        boolean[] infected = new boolean[n + 1];
-        infected[infection] = true;
+        for(int[] edge : edges){
+            int a = edge[0];
+            int b = edge[1];
+            int t = edge[2];
 
+            graph.get(a).add(new Edge(b, t));
+            graph.get(b).add(new Edge(a, t));
+        }
+
+        boolean[] infected = new boolean[n+1];
+        infected[infection] = true;
         dfs(0, infected);
 
         return answer;
     }
 
-    void dfs(int step, boolean[] infected) {
+    void dfs(int step, boolean[] infected){
+        int cnt = 0;
+        for(boolean inf : infected){
+            if(inf) cnt += 1;
+        }
+        answer = Math.max(answer, cnt);
+        if(step == k) return;
 
-        int count = 0;
-        for (int i = 1; i <= n; i++)
-            if (infected[i]) count++;
-
-        answer = Math.max(answer, count);
-
-        if (step == k) return;
-
-        for (int type = 1; type <= 3; type++) {
-
+        for(int type=0; type<=3; type++){
             boolean[] newInfected = infected.clone();
 
             Queue<Integer> q = new LinkedList<>();
+            for(int i=1; i<=n; i++){
+                if(newInfected[i]) q.add(i);
+            }
 
-            for (int i = 1; i <= n; i++)
-                if (newInfected[i])
-                    q.add(i);
-
-            while (!q.isEmpty()) {
-
+            while(!q.isEmpty()){
                 int cur = q.poll();
 
-                for (Edge next : graph.get(cur)) {
+                for(Edge next : graph.get(cur)){
+                    if(next.type != type) continue;
 
-                    if (next.type != type) continue;
-
-                    if (!newInfected[next.to]) {
+                    if(!newInfected[next.to]){
                         newInfected[next.to] = true;
                         q.add(next.to);
                     }
                 }
             }
-
-            dfs(step + 1, newInfected);
+            dfs(step+1, newInfected);
         }
     }
 }
